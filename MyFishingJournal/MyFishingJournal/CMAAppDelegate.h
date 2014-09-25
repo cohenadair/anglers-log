a869760b21dbc8d4c5be79e33fec11a0a064092d
Cohen Adair <cohenadair@gmail.com>
2014-09-24 22:22:34 -0400

    Initial Xcode project files

---
+++ MyFishingJournal/MyFishingJournal/CMAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMAAppDelegate.h
+//  MyFishingJournal
+//
+//  Created by Cohen Adair on 9/24/14.
+//  Copyright (c) 2014 Cohen Adair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFishingJournal/MyFishingJournal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyFishingJournal
+//
+//  Created by Cohen Adair on 9/24/14.
+//  Copyright (c) 2014 Cohen Adair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMAAppDelegate class]));
+    }
+}
