78fb11e8371cb98ddfe3c72176dcc5dea9a46e27
Cohen Adair <cohenadair@gmail.com>
2014-10-15 02:04:33 -0400

    Added a main Storyboard and its ViewController class

---
+++ MyFishingJournal/MyFishingJournal/ViewControllers/CMAHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMAHomeViewController.h
+//  MyFishingJournal
+//
+//  Created by Cohen Adair on 10/15/14.
+//  Copyright (c) 2014 Cohen Adair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAHomeViewController : UIViewController
+
+@end
