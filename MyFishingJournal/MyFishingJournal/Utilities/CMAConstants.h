87699b899d028a7a67f98d165bb4d51b015589f1
Cohen Adair <cohenadair@gmail.com>
2014-10-19 23:12:19 -0400

    Added new CMAViewControllerID enum values and constants representing the different sets of user defines

--- MyFishingJournal/MyFishingJournal/Utilities/CMAConstants.h
+++ MyFishingJournal/MyFishingJournal/Utilities/CMAConstants.h
@@ -2,16 +2,19 @@
 //  CMAConstants.h
 //  MyFishingJournal
 //
-//  Created by Cohen Adair on 10/16/14.
+//  Created by Cohen Adair on 10/19/14.
 //  Copyright (c) 2014 Cohen Adair. All rights reserved.
 //
 
-#ifndef MyFishingJournal_CMAConstants_h
-#define MyFishingJournal_CMAConstants_h
-
 typedef enum {
     CMAViewControllerID_Home = 1,
-    CMAViewControllerID_ViewEntries = 2
+    CMAViewControllerID_ViewEntries = 2,
+    CMAViewControllerID_EditSettings = 3,
+    CMAViewControllerID_AddEntry = 4
 } CMAViewControllerID;
 
-#endif
+extern NSString *const SET_SPECIES;
+extern NSString *const SET_BAITS;
+extern NSString *const SET_LOCATIONS;
+extern NSString *const SET_FISHING_METHODS;
+
---
+++ MyFishingJournal/MyFishingJournal/Utilities/CMAConstants.m
@@ -0,0 +1,14 @@
+//
+//  CMAConstants.m
+//  MyFishingJournal
+//
+//  Created by Cohen Adair on 10/19/14.
+//  Copyright (c) 2014 Cohen Adair. All rights reserved.
+//
+
+#import "CMAConstants.h"
+
+NSString *const SET_SPECIES = @"Species";
+NSString *const SET_BAITS = @"Baits";
+NSString *const SET_LOCATIONS = @"Locations";
+NSString *const SET_FISHING_METHODS = @"Fishing Methods";
