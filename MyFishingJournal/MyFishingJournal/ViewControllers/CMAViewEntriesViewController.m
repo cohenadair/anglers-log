46583a78e0ac5dd4aa1d0bdf77ec44cb317ab2e2
Cohen Adair <cohenadair@gmail.com>
2014-10-20 23:52:59 -0400

    Added "Add Entry" button to the home screen and removed the "+" button from the navigation bar

--- MyFishingJournal/MyFishingJournal/ViewControllers/CMAViewEntriesViewController.m
+++ MyFishingJournal/MyFishingJournal/ViewControllers/CMAViewEntriesViewController.m
@@ -31,7 +31,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return 5;
+    return 1;
 }
 
 // Sets the height of each cell.
