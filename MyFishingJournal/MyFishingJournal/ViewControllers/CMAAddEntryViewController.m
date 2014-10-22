2d7ba0b7fddd5e1f3ba308232901726aa5d87c8b
Cohen Adair <cohenadair@gmail.com>
2014-10-22 13:18:55 -0400

    Switched Add Entry table view to a static table

--- MyFishingJournal/MyFishingJournal/ViewControllers/CMAAddEntryViewController.m
+++ MyFishingJournal/MyFishingJournal/ViewControllers/CMAAddEntryViewController.m
@@ -19,41 +19,12 @@
 
 @end
 
-// Constants represent the different cell identifiers.
-NSString *const CELL_DATE_DISPLAY = @"dateDisplayCell";
-NSString *const CELL_DATE_PICKER = @"datePickerCell";
-NSString *const CELL_USER_DEFINE = @"userDefineCell";
-NSString *const CELL_USER_INPUT = @"userInputCell";
-NSString *const CELL_PICTURES = @"picturesCell";
-NSString *const CELL_NOTES = @"notesCell";
-
 @implementation CMAAddEntryViewController
 
 #pragma mark - View Management
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self.tableView setTableFooterView:[[UIView alloc] initWithFrame:CGRectZero]]; // removes empty cells at the end of the list
-    
-    self.tableLabels =
-        @{@"0" : @[@"Notes"],
-          @"1" : @[@"Methods", @"Length", @"Weight", @"Quantity", @"Bait"],
-          @"2" : @[@"Pictures"],
-          @"3" : @[@"Date & Time", @"Date Picker", @"Species", @"Location"]};
-    
-    self.tableCellTypes =
-        @{@"Date & Time" : CELL_DATE_DISPLAY,
-          @"Date Picker" : CELL_DATE_PICKER,
-          @"Species"     : CELL_USER_DEFINE,
-          @"Location"    : CELL_USER_DEFINE,
-          @"Pictures"    : CELL_PICTURES,
-          @"Methods"     : CELL_USER_DEFINE,
-          @"Length"      : CELL_USER_INPUT,
-          @"Weight"      : CELL_USER_INPUT,
-          @"Quantity"    : CELL_USER_INPUT,
-          @"Bait"        : CELL_USER_DEFINE,
-          @"Notes"       : CELL_NOTES};
 }
 
 - (void)didReceiveMemoryWarning {
@@ -62,16 +33,6 @@ NSString *const CELL_NOTES = @"notesCell";
 }
 
 #pragma mark - Table View Initializing
-
-// Returns the key of self.tableLabels at anIndex.
-- (NSString *)sectionNameAtIndex: (NSUInteger)anIndex {
-    return [[self.tableLabels allKeys] objectAtIndex:anIndex];
-}
-
-// Returns the object of self.tableLabels at anIndex.
-- (NSArray *)sectionLabelsAtIndex: (NSUInteger)anIndex {
-    return [self.tableLabels objectForKey:[self sectionNameAtIndex:anIndex]];
-}
 
 - (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     return TABLE_SECTION_SPACING;
@@ -82,58 +43,6 @@ NSString *const CELL_NOTES = @"notesCell";
         return TABLE_SECTION_SPACING;
 
     return CGFLOAT_MIN;
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return [[self.tableLabels allKeys] count];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [[self sectionLabelsAtIndex:section] count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSString *cellName = [[self sectionLabelsAtIndex:indexPath.section] objectAtIndex:indexPath.row];
-    NSString *cellType = [self.tableCellTypes objectForKey:cellName];
-
-    if ([cellType isEqualToString:CELL_USER_DEFINE]) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellType forIndexPath:indexPath];
-        cell.textLabel.text = cellName;
-        cell.detailTextLabel.text = @"None";
-        return cell;
-    }
-    
-    if ([cellType isEqualToString:CELL_USER_INPUT]) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellType forIndexPath:indexPath];
-        cell.textLabel.text = cellName;
-        return cell;
-    }
-    
-    if ([cellType isEqualToString:CELL_PICTURES]) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellType forIndexPath:indexPath];
-        cell.textLabel.text = cellName;
-        return cell;
-    }
-    
-    if ([cellType isEqualToString:CELL_DATE_DISPLAY]) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellType forIndexPath:indexPath];
-        cell.textLabel.text = cellName;
-        return cell;
-    }
-    
-    if ([cellType isEqualToString:CELL_DATE_PICKER]) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellType forIndexPath:indexPath];
-        cell.textLabel.text = cellName;
-        return cell;
-    }
-    
-    if ([cellType isEqualToString:CELL_NOTES]) {
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellType forIndexPath:indexPath];
-        cell.textLabel.text = cellName;
-        return cell;
-    }
-    
-    return nil;
 }
 
 #pragma mark - Events
