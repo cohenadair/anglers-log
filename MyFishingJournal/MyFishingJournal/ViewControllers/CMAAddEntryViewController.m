76f83bbda910d58a706758ce44eebd35230b476b
Cohen Adair <cohenadair@gmail.com>
2014-10-22 22:09:48 -0400

    Implemented a UIDatePicker for the Add Entry view

--- MyFishingJournal/MyFishingJournal/ViewControllers/CMAAddEntryViewController.m
+++ MyFishingJournal/MyFishingJournal/ViewControllers/CMAAddEntryViewController.m
@@ -11,13 +11,21 @@
 
 @interface CMAAddEntryViewController ()
 
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelButton;
+@property (weak, nonatomic)IBOutlet UIBarButtonItem *doneButton;
+@property (weak, nonatomic)IBOutlet UIBarButtonItem *cancelButton;
 
-@property (strong, nonatomic)NSDictionary *tableLabels;
-@property (strong, nonatomic)NSDictionary *tableCellTypes;
+@property (weak, nonatomic)IBOutlet UILabel *dateTimeLabel;
+@property (weak, nonatomic)IBOutlet UIDatePicker *datePicker;
+
+@property (nonatomic)BOOL isEditingDateTime;
 
 @end
+
+NSInteger const DATE_PICKER_HEIGHT = 225;
+NSInteger const DATE_PICKER_SECTION = 0;
+NSInteger const DATE_PICKER_ROW = 1;
+NSInteger const DATE_DISPLAY_SECTION = 0;
+NSInteger const DATE_DISPLAY_ROW = 0;
 
 @implementation CMAAddEntryViewController
 
@@ -25,6 +33,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.isEditingDateTime = NO;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,15 +44,59 @@
 
 #pragma mark - Table View Initializing
 
-- (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+- (void)toggleDatePickerCellHidden:(UITableView *)aTableView {
+    self.isEditingDateTime = !self.isEditingDateTime;
+    
+    // use begin/endUpdates to animate changes
+    [aTableView beginUpdates];
+    
+    if (self.isEditingDateTime)
+        [UIView animateWithDuration:0.5 animations:^{
+            [self.datePicker setAlpha:1.0f];
+        }];
+    else
+        [UIView animateWithDuration:0.15 animations:^{
+            [self.datePicker setAlpha:0.0f];
+        }];
+    
+    [aTableView reloadData];
+    
+    [aTableView endUpdates];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     return TABLE_SECTION_SPACING;
 }
 
-- (CGFloat) tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    // only set a footer for the last cell in the table
     if (section == ([self numberOfSectionsInTableView:tableView] - 1))
         return TABLE_SECTION_SPACING;
 
     return CGFLOAT_MIN;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // set height of the date picker cell
+    if (indexPath.section == DATE_PICKER_SECTION && indexPath.row == DATE_PICKER_ROW) {
+        if (self.isEditingDateTime)
+            return DATE_PICKER_HEIGHT;
+        else
+            return 0;
+    }
+    
+    // using the super class's implementation gets the height set in storyboard
+    return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // show the date picker cell when the date display cell is selected
+    if (indexPath.section == DATE_DISPLAY_SECTION && indexPath.row == DATE_DISPLAY_ROW)
+        [self toggleDatePickerCellHidden:tableView];
+    else
+        // hide the date picker cell when any other cell is selected
+        if (self.isEditingDateTime)
+            [self toggleDatePickerCellHidden:tableView];
 }
 
 #pragma mark - Events
@@ -53,6 +107,13 @@
 
 - (IBAction)clickedCancel:(UIBarButtonItem *)sender {
     [self performSegueToPreviousView];
+}
+
+- (IBAction)changedDatePicker:(UIDatePicker *)sender {
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    [dateFormatter setDateFormat:@"MMM dd, yyyy 'at' h:mm a"];
+    
+    [self.dateTimeLabel setText:[dateFormatter stringFromDate:sender.date]];
 }
 
 #pragma mark - Navigation
