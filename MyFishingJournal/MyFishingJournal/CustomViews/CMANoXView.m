a380e2214551ca2c69cf8dd264bcf25f776283f0
Cohen Adair <cohenadair@gmail.com>
2014-12-04 15:56:00 -0500

    Fixed NoXView positioning when the parent view's height includes the navigation bar

--- MyFishingJournal/MyFishingJournal/CustomViews/CMANoXView.m
+++ MyFishingJournal/MyFishingJournal/CustomViews/CMANoXView.m
@@ -12,12 +12,22 @@
 
 - (void)centerInParent:(UIView *)aParentView navigationController:(UINavigationController *)aNavigationController {
     CGRect screenFrame = [UIScreen mainScreen].bounds;
+    CGFloat navHeight = aNavigationController.navigationBar.frame.size.height;
+    CGFloat statusHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+    NSInteger yOffset = 0;
+
+    // if the parent's height includes the navigation bar
+    if (aParentView.frame.size.height == (screenFrame.size.height - navHeight - statusHeight))
+        yOffset = -(navHeight / 2) - statusHeight;
+    else
+        yOffset = -navHeight - statusHeight;
     
     CGRect newFrame;
     newFrame.origin.x = aParentView.frame.origin.x;
-    newFrame.origin.y = -aNavigationController.navigationBar.frame.size.height - [UIApplication sharedApplication].statusBarFrame.size.height;
+    newFrame.origin.y = yOffset;
     newFrame.size.width = aParentView.frame.size.width;
     newFrame.size.height = screenFrame.size.height;
+
     [self setFrame:newFrame];
 }
 
