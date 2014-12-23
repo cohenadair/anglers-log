e23e1be195e4bee4b3b10f17539aba828cc15bf7
Cohen Adair <cohen@cohens-macbook.gateway.2wire.net>
2014-12-23 18:02:47 -0500

    Added all cocoapods files

---
+++ MyFishingJournal/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 4
+
+// OpenWeatherMapAPI
+#define COCOAPODS_POD_AVAILABLE_OpenWeatherMapAPI
+#define COCOAPODS_VERSION_MAJOR_OpenWeatherMapAPI 0
+#define COCOAPODS_VERSION_MINOR_OpenWeatherMapAPI 0
+#define COCOAPODS_VERSION_PATCH_OpenWeatherMapAPI 5
+
