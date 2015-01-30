e020ddb96482f6676f68bd334b297b1351ecc631
Coh3n <cohenadair@gmail.com>
2015-01-29 19:55:58 -0500

    Disabled core data WAL since the file was growing at an alarming rate

--- TheAnglersLog/TheAnglersLog/ApplicationModel/CMAStorageManager.m
+++ TheAnglersLog/TheAnglersLog/ApplicationModel/CMAStorageManager.m
@@ -322,7 +322,9 @@
     NSError *error = nil;
     NSString *failureReason = @"There was an error creating or loading the application's saved data.";
     
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+    NSDictionary *options = @{NSSQLitePragmasOption: @{@"journal_mode" : @"DELETE"}};
+    
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
         // Report any error we got.
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
         dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
