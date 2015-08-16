9c05a1073a0034fedad61166c1e49f73aba0ae08
Cohen Adair <cohenadair@gmail.com>
2015-08-15 21:06:55 -0400

    CatchesFragment now inherits from ListFragment to clean things up a bit.

--- AnglersLog/app/src/main/java/com/cohenadair/anglerslog/fragments/CatchesFragment.java
+++ AnglersLog/app/src/main/java/com/cohenadair/anglerslog/fragments/CatchesFragment.java
@@ -1,34 +1,39 @@
 package com.cohenadair.anglerslog.fragments;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.cohenadair.anglerslog.R;
 import com.cohenadair.anglerslog.model.Catch;
 import com.cohenadair.anglerslog.model.Logbook;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class CatchesFragment extends Fragment {
+public class CatchesFragment extends ListFragment {
 
     public CatchesFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View aView = inflater.inflate(R.layout.fragment_catches, container, false);
+        View aView = super.onCreateView(inflater, container, savedInstanceState);
 
         // set the list's adapter
         ArrayAdapter adapter = new ArrayAdapter<Catch>(this.getActivity(), android.R.layout.simple_list_item_1, Logbook.getSharedLogbook().getCatches());
-        ListView listView = (ListView)aView.findViewById(R.id.allCatchesListView);
-        listView.setAdapter(adapter);
+        setListAdapter(adapter);
 
         return aView;
+    }
+
+    @Override
+    public void onListItemClick(ListView aListView, View aView, int aPos, long id) {
+        Toast.makeText(getActivity(), "Test", Toast.LENGTH_SHORT).show();
     }
 }
