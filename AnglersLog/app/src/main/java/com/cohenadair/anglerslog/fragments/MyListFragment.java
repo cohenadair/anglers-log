cb9ecc6ae7b95520671fb40b5b455efad1f30c56
Cohen Adair <cohenadair@gmail.com>
2015-09-06 21:16:43 -0400

    Removed a Log.d() call

--- AnglersLog/app/src/main/java/com/cohenadair/anglerslog/fragments/MyListFragment.java
+++ AnglersLog/app/src/main/java/com/cohenadair/anglerslog/fragments/MyListFragment.java
@@ -5,7 +5,6 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -114,8 +113,6 @@ public class MyListFragment extends Fragment {
 
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
-                Log.d("OnScrollStateChanged", "State: " + scrollState);
-
                 if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE)
                     mNewButton.show();
                 else if (mDidScroll) {
