002e8ce327cb72e227b8f79cf1244386706da61b
Cohen Adair <cohenadair@gmail.com>
2016-02-07 18:52:52 -0500

    Importer now imports images successfully and reads in the JSON data. The JSON now needs to be parsed, and a Logbook needs to be constructed.

--- AnglersLog/app/src/main/java/com/cohenadair/anglerslog/model/Importer.java
+++ AnglersLog/app/src/main/java/com/cohenadair/anglerslog/model/Importer.java
@@ -3,10 +3,19 @@ package com.cohenadair.anglerslog.model;
 import android.content.ContentResolver;
 import android.net.Uri;
 import android.os.Handler;
-import android.util.Log;
 
+import com.cohenadair.anglerslog.utilities.PhotoUtils;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -37,7 +46,7 @@ public class Importer {
         new Thread(new UnzipRunnable()).start();
     }
 
-    private static void iterateZip() throws IOException {
+    private static void importData() throws IOException {
         InputStream is = mContentResolver.openInputStream(mUri);
         if (is == null)
             return;
@@ -46,8 +55,15 @@ public class Importer {
         ZipEntry entry;
 
         while ((entry = zip.getNextEntry()) != null) {
-            Log.i(TAG, entry.getName());
+            String name = entry.getName();
+
+            if (name.endsWith(".json"))
+                importLogbook(zip, entry);
+            //else
+                //importImage(zip, entry);
         }
+
+        zip.close();
 
         if (mCallbacks != null)
             mHandler.post(new Runnable() {
@@ -58,11 +74,49 @@ public class Importer {
             });
     }
 
+    private static void importLogbook(ZipInputStream in, ZipEntry entry) throws IOException {
+        StringBuilder jsonStr = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        // read in the JSON string
+        String next;
+        while ((next = reader.readLine()) != null)
+            jsonStr.append(next);
+
+        // convert String to JSON
+        try {
+            JSONObject json = new JSONObject(jsonStr.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Copies the image represented by the given ZipEntry to the applications pictures folder.
+     *
+     * @param in The InputStream being read from.
+     * @param entry The ZipEntry containing the image to copy.
+     */
+    private static void importImage(ZipInputStream in, ZipEntry entry) {
+        File newFile = PhotoUtils.privatePhotoFile(entry.getName());
+
+        if (newFile == null || newFile.exists())
+            return;
+
+        try {
+            FileOutputStream out = new FileOutputStream(newFile);
+            IOUtils.copy(in, out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static class UnzipRunnable implements Runnable {
         @Override
         public void run() {
             try {
-                iterateZip();
+                importData();
             } catch (IOException e) {
                 e.printStackTrace();
             }
