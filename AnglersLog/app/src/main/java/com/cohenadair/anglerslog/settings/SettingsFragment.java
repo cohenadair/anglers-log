67bd1bd21688d92d1999e5fed177e7636a98323c
Cohen Adair <cohenadair@gmail.com>
2016-03-18 09:43:53 -0400

    Removed settings list separators

--- AnglersLog/app/src/main/java/com/cohenadair/anglerslog/settings/SettingsFragment.java
+++ AnglersLog/app/src/main/java/com/cohenadair/anglerslog/settings/SettingsFragment.java
@@ -2,6 +2,8 @@ package com.cohenadair.anglerslog.settings;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
@@ -57,6 +59,10 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onResume() {
         super.onResume();
         showImportDialog();
+
+        // hide list separators
+        setDivider(new ColorDrawable(Color.TRANSPARENT));
+        setDividerHeight(0);
     }
 
     @Override
