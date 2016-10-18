c6b8621529174de2b1c5badd893a8b43eb40178e
Cohen Adair <cohenadair@gmail.com>
2016-10-18 19:03:04 -0400

    iOS updates for Xcode 8

--- ios/AnglersLog/CustomViews/CMABaitTableViewCell.m
+++ ios/AnglersLog/CustomViews/CMABaitTableViewCell.m
@@ -11,6 +11,7 @@
 @implementation CMABaitTableViewCell
 
 - (void)awakeFromNib {
+    [super awakeFromNib];
     // Initialization code
 }
 
--- ios/AnglersLog/CustomViews/CMAEntryTableViewCell.m
+++ ios/AnglersLog/CustomViews/CMAEntryTableViewCell.m
@@ -11,6 +11,7 @@
 @implementation CMAEntryTableViewCell
 
 - (void)awakeFromNib {
+    [super awakeFromNib];
     // Initialization code
 }
 
