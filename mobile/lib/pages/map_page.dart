386587a91a36e79bf599f848d797cd90de01df2b
cohenadair <cohenadair@gmail.com>
2019-10-09 21:35:21 -0400

    #178: Show marker and bottom sheet on long press of map

--- mobile/lib/pages/map_page.dart
+++ mobile/lib/pages/map_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobile/app_manager.dart';
+import 'package:mobile/res/dimen.dart';
 import 'package:mobile/widgets/page.dart';
+import 'package:mobile/widgets/widget.dart';
+import 'package:uuid/uuid.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   final AppManager app;
 
   MapPage({
@@ -11,15 +14,84 @@ class MapPage extends StatelessWidget {
   }) : assert(app != null);
 
   @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Set<Marker> _markers = Set();
+  Marker _activeMarker;
+
+  @override
   Widget build(BuildContext context) {
     return Page(
-      child: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.42796133580664, -122.085749655962),
-          zoom: 15,
-        ),
-        myLocationButtonEnabled: false,
+      child: Stack(
+        children: <Widget>[
+          _buildMap(),
+          _buildBottomSheet(),
+        ],
       ),
     );
+  }
+
+  Widget _buildMap() => GoogleMap(
+    markers: _markers,
+    initialCameraPosition: CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 15,
+    ),
+    myLocationButtonEnabled: false,
+    onLongPress: (LatLng latLng) {
+      _addMarker(latLng);
+    },
+  );
+
+  Widget _buildBottomSheet() {
+    if (_activeMarker == null) {
+      return Empty();
+    }
+
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        color: Colors.white,
+        padding: insetsDefault,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Wrap(
+              direction: Axis.vertical,
+              children: <Widget>[
+                Text(_activeMarker.position.latitude.toString()),
+                Text(_activeMarker.position.longitude.toString()),
+              ],
+            ),
+            IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () => _updateActiveMarker(null),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _addMarker(LatLng latLng) {
+    setState(() {
+      MarkerId markerId = MarkerId(Uuid().v1().toString());
+      _activeMarker = Marker(
+        markerId: markerId,
+        position: latLng,
+        onTap: () => _updateActiveMarker(_markers
+            .firstWhere((Marker marker) => marker.markerId == markerId)),
+      );
+      _markers.add(_activeMarker);
+    });
+  }
+
+  void _updateActiveMarker(Marker marker) {
+    setState(() {
+      _activeMarker = marker;
+    });
   }
 }
