42e2d434b1ef27777e81c8f895f32d54eb296d10
cohenadair <cohenadair@gmail.com>
2020-08-27 08:28:27 -0400

    Fixed #434: FAB doesn't reappear after scrolling down

--- android/app/src/main/java/com/cohenadair/anglerslog/utilities/ScrollingFabBehavior.java
+++ android/app/src/main/java/com/cohenadair/anglerslog/utilities/ScrollingFabBehavior.java
@@ -20,16 +20,28 @@ public class ScrollingFabBehavior extends FloatingActionButton.Behavior {
     }
 
     @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+            FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes)
+    {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(
+                coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
     }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+            View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed)
+    {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed);
 
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE)
-            child.hide();
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
         else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE)
             child.show();
     }
