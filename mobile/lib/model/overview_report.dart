8e5088da479750a979050b4ea37a34d3e1a93586
cohenadair <cohenadair@gmail.com>
2020-11-22 12:07:53 -0500

    Fixed #454: Exception rebuilding stats page for OverviewReport

--- mobile/lib/model/overview_report.dart
+++ mobile/lib/model/overview_report.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../i18n/strings.dart';
+import 'gen/anglerslog.pb.dart';
 
 /// A pre-defined report that gives an overview of a users log data.
 ///
@@ -8,7 +9,7 @@ import '../i18n/strings.dart';
 /// * [OverviewReportView].
 @immutable
 class OverviewReport {
-  String get id => "overview_report";
+  Id get id => Id()..uuid = "fbcc462b-139e-4a8e-9955-d0fb97071d58";
 
   String title(BuildContext context) =>
       Strings.of(context).statsPageReportOverview;
