1081fb22c46938565346800e99b5d3a24cde14b5
cohenadair <cohenadair@gmail.com>
2021-02-20 13:14:58 -0500

    #473: Link directly to App Store when rating app

--- mobile/lib/utils/store_utils.dart
+++ mobile/lib/utils/store_utils.dart
@@ -7,7 +7,7 @@ import 'snackbar_utils.dart';
 
 String _storeUrl() => Platform.isAndroid
     ? "https://play.google.com/store/apps/details?id=com.cohenadair.anglerslog"
-    : "https://apps.apple.com/app/id959989008?action=write-review";
+    : "itms-apps://apps.apple.com/app/id959989008?action=write-review";
 
 Future<void> launchStore(BuildContext context) async {
   var launcher = UrlLauncherWrapper.of(context);
